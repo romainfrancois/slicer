d05ca5d18bd0f5c8a52fa3c0d6d62abcc0e2a0fe
Romain Francois <romain@purrple.cat>
2019-03-07 22:26:16 +0100

    docuemnt()

--- R/slicer.R
+++ R/slicer.R
@@ -2,9 +2,14 @@ is_bare_vector <- function(x) {
   is_vector(x) && !is.object(x) && is.null(attr(x, "class"))
 }
 
+#' slicer
+#'
+#' @param x A vector
+#' @return a function
+#'
 #' @export
-slicer <- function(.) {
-  if (is_bare_vector(.)) {
+slicer <- function(x) {
+  if (is_bare_vector(x)) {
     .subset
   } else {
     vec_slice
